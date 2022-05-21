32b55ac9743ef4e7b7a919dcac7146c79fa3aeeb
baza04 <bazarbay.amir@gmail.com>
2022-05-21 15:42:20 +0600

    solution: hw_task4.1

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,12 @@
 package reverse_string
 
-func ReverseString(input string) (output string) {
-	// solution goes here
-	return output
+func ReverseString(input string) string {
+	ln := len(input)
+	buff := make([]rune, ln)
+
+	for i, c := range input {
+		buff[ln-1-i] = c
+	}
+
+	return string(buff)
 }
