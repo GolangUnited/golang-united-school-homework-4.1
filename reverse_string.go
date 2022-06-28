9aa0857720a6a62712f0bcafa10f6f2677ac878d
amarnath reddy <learninggona@gmail.com>
2022-06-28 06:33:54 +0530

    init commit

--- reverse_string.go
+++ reverse_string.go
@@ -2,5 +2,13 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	// solution goes here
+	if len(input) == 0 {
+		return ""
+	}
+
+	smallOutput := ReverseString(input[1:])
+	myRune := []rune(input)
+	output = smallOutput + string(myRune[0])
+
 	return output
 }
